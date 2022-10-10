aa2a30cd1c16b43a2e14d855e15739bc7330954c
samuciapp03 <samu.ciappesoni@gmail.com>
2022-10-10 15:45:27 +0200

    fine_esercitazioni1

--- src/main/java/it/eforhum/corsojava/jdbc/MainDemo.java
+++
@@ -1,9 +0,0 @@
-package it.eforhum.corsojava.jdbc;
-
-import it.eforhum.corsojava.jdbc.service.UtenteService;
-
-public class MainDemo {
-	public static void main(String[] args) {
-		System.out.println(UtenteService.getInstance().findById(1));
-	}
-}
